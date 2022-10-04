27b6500d086a40bc0c960c790cf1c3178f46aab1
Arthur <arthurvitorgn@gmail.com>
2022-10-04 20:02:25 -0300

    New style

--- paginas/urls.py
+++ paginas/urls.py
@@ -3,9 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('about/', views.about, name='sobre'),
-    path('services/', views.services, name='servicos'),
-    path('prices/', views.prices, name='precos'),
     path('blog/', views.blog, name='blog'),
     path('detail/', views.detail, name='detail'),
 ]
--- paginas/views.py
+++ paginas/views.py
@@ -6,18 +6,6 @@ def index(request):
     return render(request, 'paginas/index.html')
 
 
-def about(request):
-    return render(request, 'paginas/sobre.html')
-
-
-def services(request):
-    return render(request, 'paginas/servicos.html')
-
-
-def prices(request):
-    return render(request, 'paginas/precos.html')
-
-
 def blog(request):
     return render(request, 'paginas/blog.html')
 
